e89ddd265af0145d2fb4c9071f9fc21344d2fc78
kaviska <126580314+kaviska@users.noreply.github.com>
2025-04-30 21:36:09 +0530

    updated

--- src/compoments/BestSellers.tsx
+++ src/compoments/BestSellers.tsx
@@ -50,7 +50,7 @@ export default function BestSellers() {
             <div className="mt-8">
                 <h2 className="font-medium text-[30px] md:mx-8 text-center md:text-start mb-8">Best Sellers</h2>
             
-                <div className="flex  md:flex-row md:gap-0 gap-6 flex-col flex-wrap md:px-8 px-4">
+                <div className="flex  md:flex-row md:gap-0 gap-6 flex-col flex-wrap md:px-8 px-2">
                     {isMobileView ? (
                         <Swiper
                             cssMode={true}
